608f07efe31107f63367a4c30d065b1031671230
Daniel Cruickshanks <dankom@live.de>
2019-12-17 09:29:52 +0000

    (wip): created comment box with change event handler to set state, need to add submit functionality and pass username through props

--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
 
 export default class NewCommentForm extends Component {
+    state = {
+        comment: ''
+    };
+
+    handleChange = ({ target }) => {
+        this.setState({ comment: target.value });
+    };
+
     render() {
         return (
-            <div>
-                NewCommentForm
-            </div>
+            <form noValidate autoComplete="off">
+                <div>
+                    <TextField
+                    id="filled-multiline-static"
+                    label="Comment"
+                    multiline
+                    rows="4"
+                    value={this.state.comment}
+                    variant="filled"
+                    onChange={this.handleChange}
+                    />
+                </div>
+            </form>
         );
     };
 };
